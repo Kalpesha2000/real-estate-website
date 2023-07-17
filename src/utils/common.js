4e726c237f3f9b9cd7c74c5c8243344cba1e2a68
Kalpesha2000 <pingalekalpesha@gmail.com>
2023-07-17 12:04:30 +0530

    first commit

---
+++ src/utils/common.js
@@ -0,0 +1,18 @@
+export const sliderSettings = {
+  sliderPerView: 1,
+  spaceBetween: 50,
+  breakpoint: {
+    480: {
+      slidesPerView: 1,
+    },
+    600: {
+      slidersPerView: 2,
+    },
+    750: {
+      sliderPerView: 3,
+    },
+    1100: {
+      sliderPerView: 4,
+    },
+  },
+};
